09d26bcbc563cc5dd69fbe0b64faf08fbdc79013
Vansh Vardhan Singh <vanshvsingh@gmail.com>
2024-02-22 19:04:07 +0530

    Update example.spec.ts

--- samples/get-started/tests/example.spec.ts
+++ samples/get-started/tests/example.spec.ts
@@ -1,15 +1,15 @@
 import { test, expect } from '@playwright/test';
 
 // This sample simulates a larger test suite
-const TEST_ITERATIONS = parseInt(process.env.TEST_ITERATIONS || "0");
+const TEST_ITERATIONS = parseInt(process.env.TEST_ITERATIONS || "1");
 for (var i = 0; i < TEST_ITERATIONS; i++) {
 
-  test('has title ' + i, async ({ page }) => {
-    await page.goto('https://playwright.dev/');
+  // test('has title ' + i, async ({ page }) => {
+  //   await page.goto('https://playwright.dev/');
 
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Playwright/);
-  });
+  //   // Expect a title "to contain" a substring.
+  //   await expect(page).toHaveTitle(/Playwright/);
+  // });
 
   test('get started link ' + i, async ({ page }) => {
     await page.goto('https://playwright.dev/');
