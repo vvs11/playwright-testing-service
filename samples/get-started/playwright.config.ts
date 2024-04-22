68094ab4a5b8e72e06efd9c593333562952ddf20
Vansh Vardhan Singh <vanshvsingh@gmail.com>
2024-04-22 20:12:54 +0530

    Update playwright.config.ts

--- samples/get-started/playwright.config.ts
+++ samples/get-started/playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: [['html'],['blob'],['github'],['list'], ['@microsoft/mpt-reporter']],
+  reporter: [['html'],['blob'],['github'],['list']],
   use: {
     trace: 'on-first-retry',
     video:'retain-on-failure',
